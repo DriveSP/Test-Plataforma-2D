f4e6a4f2f524d75bbffc37e9e240a9aa1392d321
DriveSP <gtavvice@hotmail.es>
2024-10-09 09:10:47 +0200

    Some changes
    Some changes

--- Assets/Scripts/killzone.gd
+++ Assets/Scripts/killzone.gd
@@ -5,7 +5,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("Test fall")
 	Engine.time_scale = 0.5
-	body.get_node("PlayerSprite").play("dead")
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
