4d7451b51add2ab75acaede02fa372f1185225db
DriveSP <gtavvice@hotmail.es>
2024-10-30 22:11:28 +0100

    test
    test

--- projectile.gd
+++ projectile.gd
@@ -12,5 +12,7 @@ func _physics_process(delta):
 	global_position += direction * speed * delta
 		
 func _on_body_entered(body: Node2D) -> void:
+	print("collision")
 	if body.is_in_group("enemy"):
+		print("enemy collision")
 		body.queue_free()
